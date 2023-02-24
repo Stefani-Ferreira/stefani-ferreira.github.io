a7c2f3551a57b6b7f12d66388f97f18ee9033644
Stefani-Ferreira <stefaniferreira.sf.sf@gmail.com>
2023-02-24 18:56:02 -0300

    first push

---
+++ portfolio/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import SocialNetworks from './SocialNetworks';
+import InformationContainer from './InformationContainer';
+
+import Avatar from '../img/foto-perfil.png'
+
+import '../styles/components/sidebar.sass';
+const Sidebar = () => {
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt='Stéfani Ferreira'/>
+      <p className="title">Desenvolvedor</p>
+      <SocialNetworks/>
+      <InformationContainer />
+      <a href="" className="btn">Download Currículo</a>
+    </aside>
+  )
+}
+export default Sidebar
